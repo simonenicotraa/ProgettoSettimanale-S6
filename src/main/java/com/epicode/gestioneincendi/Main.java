0e4dd04b7976c046a4aec35d61a336f5eab4371c
simonenicotraa <simone.nicotra95@gmail.com>
2022-09-21 19:20:37 +0200

    commit finale

--- src/main/java/com/epicode/gestioneincendi/Main.java
+++ src/main/java/com/epicode/gestioneincendi/Main.java
@@ -1,6 +1,7 @@
 package com.epicode.gestioneincendi;
 
 import com.epicode.gestioneincendi.centrocontrollo.CentroDiControlloConcreta;
+import com.epicode.gestioneincendi.centrocontrollo.CentroDiControlloFactory;
 import com.epicode.gestioneincendi.sonde.Observer;
 import com.epicode.gestioneincendi.sonde.Sonda;
 
@@ -11,13 +12,17 @@ public class Main {
 		Observer observer1 = new CentroDiControlloConcreta();
 		Observer observer2 = new CentroDiControlloConcreta();
 		
+		
+		CentroDiControlloFactory factory = new CentroDiControlloFactory();
+		
+		
 		Sonda sonda = new Sonda();
 		sonda.setId((long) 1);
 		sonda.setLatitudine("11,22,33");
 		sonda.setLongitudine("11,22,33");
 		sonda.setLivelloDiFumo(1);
 		
-		sonda.aggiungiObserver(observer1);
+		sonda.aggiungiObserver((Observer) factory.creaCentroDiControllo());
 		sonda.aggiungiObserver(observer2);
 		sonda.rilevaLivelloFumo();
 		sonda.setLivelloDiFumo(6);
